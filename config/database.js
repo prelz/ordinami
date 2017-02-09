97eb7263d6d515ead6464e4b4ae8752b6020bb92
prelz <francesco.prelz@mi.infn.it>
2017-02-09 10:38:11 +0100

    Database config.

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+  production: {
+    host     : 'CHANGEME.XY.infn.it',
+    user     : 'CHANGEME-user',
+    insecureAuth: true, // CHANGEME ?
+    password : 'CHANGEME',
+    database : 'CHANGEME-ordini'
+  }
+};
